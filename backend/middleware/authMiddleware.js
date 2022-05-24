4249bd52fd43584d717e4df3120168dc820653d0
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-05-24 17:10:30 +0600

    auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      next()
+    } catch (error) {
+      console.log(error)
+      res.status(401).json({ message: 'Unauthorized' })
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'Private Route, Provide Token' })
+  }
+})
+
+module.exports = { protect }
