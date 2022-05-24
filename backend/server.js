02413f648208f0bb36268d374358aa52c26fcbaf
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-05-24 22:12:18 +0600

    ++ cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const dotenv = require('dotenv').config();
+const express = require('express');
+var cors = require('cors')
 const { connectToDB } = require('./config/db');
 
 
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 connectToDB();
 const app = express() ;
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
