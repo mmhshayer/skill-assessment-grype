b7ab5d6141d4e4477e8f8f26d83d807189f45c56
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-05-24 17:10:40 +0600

    applied auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const { getAllUsers } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
-router.get('/', getAllUsers);
+router.get('/', protect, getAllUsers);
 
 module.exports = router;
