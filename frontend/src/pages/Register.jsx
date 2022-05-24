6d91444b8d031ef6ac3d515865fd597b161e5f6d
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-05-24 22:13:56 +0600

    register service functional

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { register, reset } from '../features/auth/authSlice'
+import Loading from '../components/Loading';
 import { FaUserPlus } from 'react-icons/fa';
 
 function Register() {
@@ -7,19 +12,59 @@ function Register() {
     last_name: '',
     email: '',
     password: '',
+    password2: '',
     role: '',
-  });
+  })
 
-  const { first_name, last_name, email, password, password2, role } = formData;
+  const { first_name, last_name, email, password, password2, role } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-    }));
+    }))
   }
 
-  const onSubmit = (e) => {}
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== password2) {
+      toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        first_name,
+        last_name,
+        email,
+        password,
+        role,
+      }
+
+      dispatch(register(userData))
+    }
+  }
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <>
@@ -27,77 +72,77 @@ function Register() {
         <h1>
           <FaUserPlus /> Register
         </h1>
-        <p>Sign up with Us</p>
+        <p>Sign up with us</p>
       </section>
 
       <section className='form'>
         <form onSubmit={onSubmit}>
           <div className='form-group'>
-            <input 
-              type="text"
+            <input
+              type='text'
               className='form-control'
               id='first_name'
-              placeholder='First Name'
+              name='first_name'
               value={first_name}
+              placeholder='First name'
               onChange={onChange}
             />
           </div>
-
           <div className='form-group'>
             <input
-              type="text"
+              type='text'
               className='form-control'
               id='last_name'
-              placeholder='Last Name'
+              name='last_name'
               value={last_name}
+              placeholder='Last name'
               onChange={onChange}
             />
-          </div>
-
+          </div>          
           <div className='form-group'>
             <input
-              type="email"
+              type='email'
               className='form-control'
               id='email'
-              placeholder='Email'
+              name='email'
               value={email}
+              placeholder='Enter your email'
               onChange={onChange}
             />
           </div>
-
           <div className='form-group'>
             <input
-              type="password"
+              type='password'
               className='form-control'
               id='password'
-              placeholder='Password'
+              name='password'
               value={password}
+              placeholder='Enter password'
               onChange={onChange}
             />
           </div>
-
           <div className='form-group'>
             <input
-              type="password"
+              type='password'
               className='form-control'
               id='password2'
-              placeholder='Confirm Password'
+              name='password2'
               value={password2}
+              placeholder='Confirm password'
               onChange={onChange}
             />
           </div>
-
           <div className='form-group'>
             <input
-              type="text"
+              type='text'
               className='form-control'
               id='role'
-              placeholder='Role'
+              name='role'
               value={role}
+              placeholder='Role'
               onChange={onChange}
             />
           </div>
-
           <div className='form-group'>
             <button type='submit' className='btn btn-block'>
               Submit
