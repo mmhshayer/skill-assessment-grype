bfa11898bd43a6b47fb108e74841328daf2da87d
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-05-25 09:56:33 +0600

    ++ sort members and added check for "Members"

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 const asyncHandler = require('express-async-handler')
 
 const getAllMembers = asyncHandler(async (req, res) => {
-    const members = await User.find({ role: 'member' }).select('-password').exec();
+    const members = await User.find({ role: 'member' || 'Members' }).sort({ createdAt: -1 }).select('-password').exec();
     res.status(200).json(members);
 })
 
