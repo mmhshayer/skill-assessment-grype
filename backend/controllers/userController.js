8728274f5bb5bba264706fc45cafecf6d5f84cee
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-05-25 09:32:28 +0600

    bug fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler')
 
 const getAllMembers = asyncHandler(async (req, res) => {
     const members = await User.find({ role: 'member' }).select('-password').exec();
-    res.status(200).json({ members });
+    res.status(200).json(members);
 })
 
 module.exports = {
